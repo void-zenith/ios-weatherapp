6b45bb9cbe80cfc4895045ce2fd5044f714323ab
void-zenith <zenithrajbhandari@gmail.com>
2024-03-31 23:14:11 -0400

    Initial Commit

---
+++ zenithrajbhandari_assign2/zenithrajbhandari_assign2App.swift
@@ -0,0 +1,17 @@
+//
+//  zenithrajbhandari_assign2App.swift
+//  zenithrajbhandari_assign2
+//
+//  Created by zenith mac on 2024-03-31.
+//
+
+import SwiftUI
+
+@main
+struct zenithrajbhandari_assign2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
